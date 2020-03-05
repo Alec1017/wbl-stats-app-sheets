670cc358c82aac4f7b3ae33a73ab2ace058a8abb
Alec DiFederico <alecdifederico@gmail.com>
2020-03-04 22:08:07 -0500

    turned sheets functions into a properly working api

---
+++ app/__init__.py
@@ -0,0 +1,61 @@
+from __future__ import print_function, division
+from flask import Flask
+
+import pickle
+from dotenv import load_dotenv
+import os
+import os.path
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+cred = credentials.Certificate('firebase-credentials.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# If modifying these scopes, delete the file token.pickle.
+scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID and range of a sample spreadsheet.
+spreadsheet_id = os.getenv("SPREADSHEET_ID")
+range_name = 'Sheet1'
+
+def authenticate_sheet():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'sheets-credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    return service.spreadsheets()
+    
+
+sheet = authenticate_sheet()
+
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,188 @@
+from app import app, db, sheet, spreadsheet_id, range_name
+
+
+def calcHits(singles, doubles, triples, home_runs):
+  return singles + doubles + triples + home_runs
+
+def calcAtBats(hits, outs):
+  return hits + outs
+
+def calcOBP(hits, at_bats, base_on_balls, hit_by_pitch):
+  denom = at_bats + base_on_balls + hit_by_pitch
+  if denom == 0:
+    return 'Undefined'
+
+  return round(float((hits + base_on_balls + hit_by_pitch) / denom), 3)
+
+def calcAVG(hits, at_bats):
+  if at_bats == 0:
+    return 'Undefined'
+
+  return round(float(hits / at_bats), 3)
+
+def calcSLG(singles, doubles, triples, home_runs, at_bats):
+  if at_bats == 0:
+    return 'Undefined'
+
+  return round(float((singles + (2 * doubles) + (3 * triples) + (4 * home_runs)) / at_bats), 3)
+
+def calcOPS(obp, slg):
+  if obp == 'Undefined' or slg == 'Undefined':
+    return 'Undefined'
+
+  return round(float(obp + slg), 3) if obp + slg != 0 else 'Undefined'
+
+def calcERA(earned_runs, innings_pitched):
+  if innings_pitched == 0:
+    return 'Undefined'
+
+  return round(float((earned_runs * 3) / innings_pitched), 2)
+
+
+def build_sheet():
+  # get firebase users
+  users = db.collection(u'users').stream()
+  name_row = ['Player', 'H', 'AB', 'OBP', 'AVG', 'SLG', 'OPS', '1B', '2B', '3B', 'HR', 'HBP', 'BB', 'RBI', 'K', 'SB', 'OUTS', 'GP', '', 'IP', 'ER', 'R', 'K', 'BB', 'SV', 'W', 'L', 'ERA']
+  values = [name_row, []]
+
+  for user in users:
+    player = user.to_dict()
+
+    first_name = player.get('firstName')
+    last_name = player.get('lastName')
+    full_name = u'{} {}'.format(first_name, last_name)
+    
+
+    games = db.collection(u'games').where(u'player', u'==', full_name).stream()
+    
+    hits = 0
+    at_bats = 0
+    on_base_percentage = 0
+    slugging = 0
+    on_base_plus_slugging = 0
+    average = 0
+    singles = 0
+    doubles = 0
+    triples = 0
+    home_runs = 0
+    hit_by_pitch = 0
+    base_on_balls = 0
+    runs_batted_in = 0
+    strikeouts = 0
+    stolen_bases = 0
+    outs = 0
+    games_played = 0
+
+    innings_pitched = 0
+    earned_runs = 0
+    runs = 0
+    pitching_strikeouts = 0
+    pitching_base_on_balls = 0
+    saves = 0
+    wins = 0
+    losses = 0
+
+    sheet_row = []
+
+    # Summing up all the stats for a player
+    for game in games: 
+      stats = game.to_dict()
+
+      singles += stats.get('singles')
+      doubles += stats.get('doubles')
+      triples += stats.get('triples')
+      home_runs += stats.get('homeRuns')
+      hit_by_pitch += stats.get('hitByPitch')
+      base_on_balls += stats.get('baseOnBalls')
+      runs_batted_in += stats.get('runsBattedIn')
+      strikeouts += stats.get('strikeouts')
+      stolen_bases += stats.get('stolenBases')
+      outs += stats.get('outs')
+      games_played += 1
+
+      innings_pitched += stats.get('inningsPitched')
+      earned_runs += stats.get('earnedRuns')
+      runs += stats.get('runs')
+      pitching_strikeouts += stats.get('pitchingStrikeouts')
+      pitching_base_on_balls += stats.get('pitchingBaseOnBalls')
+      saves += stats.get('saves')
+      wins += stats.get('win')
+      losses += stats.get('loss')
+
+    # Calculate stats
+    hits = calcHits(singles, doubles, triples, home_runs)
+    at_bats = calcAtBats(hits, outs)
+    on_base_percentage = calcOBP(hits, at_bats, base_on_balls, hit_by_pitch)
+    average = calcAVG(hits, at_bats)
+    slugging = calcSLG(singles, doubles, triples, home_runs, at_bats)
+    on_base_plus_slugging = calcOPS(on_base_percentage, slugging)
+    earned_run_average = calcERA(earned_runs, innings_pitched)
+
+    # append stats to row
+    sheet_row.append(full_name)
+    sheet_row.append(hits)
+    sheet_row.append(at_bats)
+    sheet_row.append(on_base_percentage)
+    sheet_row.append(average)
+    sheet_row.append(slugging)
+    sheet_row.append(on_base_plus_slugging)
+    sheet_row.append(singles)
+    sheet_row.append(doubles)
+    sheet_row.append(triples)
+    sheet_row.append(home_runs)
+    sheet_row.append(hit_by_pitch)
+    sheet_row.append(base_on_balls)
+    sheet_row.append(runs_batted_in)
+    sheet_row.append(strikeouts)
+    sheet_row.append(stolen_bases)
+    sheet_row.append(stolen_bases)
+    sheet_row.append(games_played)
+
+    sheet_row.append('')
+
+    sheet_row.append(innings_pitched)
+    sheet_row.append(earned_runs)
+    sheet_row.append(runs)
+    sheet_row.append(pitching_strikeouts)
+    sheet_row.append(pitching_base_on_balls)
+    sheet_row.append(saves)
+    sheet_row.append(wins)
+    sheet_row.append(losses)
+    sheet_row.append(earned_run_average)
+
+    values.append(sheet_row)
+    values.append([])
+
+  return values
+
+
+def clear_sheet():
+  try:
+    sheet.values().batchClear(spreadsheetId=spreadsheet_id, body={'ranges': [range_name]}).execute()
+  except:
+    print('Sheet was not cleared successfully')
+  else:
+    print('Sheet cleared successfully!')
+
+
+def update_sheet(db_values):
+  return_value = ''
+  try:
+    result = sheet.values().update(
+      spreadsheetId=spreadsheet_id, range=range_name,
+      valueInputOption='USER_ENTERED', body={'values': db_values}).execute()
+    print('Success! {0} cells updated.'.format(result.get('updatedCells')))
+    return_value = 'Success'
+  except:
+    print('Something went wrong! Stats were not uploaded to sheet')
+    return_value = 'Failure'
+
+  return return_value
+  
+
+@app.route('/api/update_sheet')
+def api():
+  values = build_sheet()
+  clear_sheet()
+
+  return update_sheet(values)
