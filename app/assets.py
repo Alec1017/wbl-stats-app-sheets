1faa610a2eebe4545e527d3f7560ed7687fc56f5
Alec DiFederico <alecdifederico@gmail.com>
2021-02-14 19:05:30 -0500

    added scss bundle

--- app/assets.py
+++ app/assets.py
@@ -2,9 +2,10 @@ from flask_assets import Bundle
 
 bundles = {
     'main_css': Bundle(
-        'scss/main.scss',
+        'scss/mixins/*.scss',
+        'scss/pages/*.scss',
+        'scss/*.scss',
         depends='scss/**/*.scss',
-        output='css/main.%(version)s.css',
-
+        output='css/main.%(version)s.css'
     )
 }
